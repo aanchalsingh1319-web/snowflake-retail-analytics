889e8cef66d8732f4dbaca2fdfd92c20c246385e
aanchalsingh1319-web <aanchalsingh1319@gmail.com>
2026-01-29 19:41:52 +0530

    Create 05_analytics_queries.sql

---
+++ sql/05_analytics_queries.sql
@@ -0,0 +1,8 @@
+-- Business analytics queries
+
+-- Daily revenue trend
+-- Monthly revenue trend
+-- Top products by revenue
+-- Revenue by region
+-- Repeat vs new customers
+-- Average order value (AOV)
