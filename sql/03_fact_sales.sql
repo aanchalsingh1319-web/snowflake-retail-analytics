1b492be6f763634f4033ea58e57e6a646e58f816
aanchalsingh1319-web <aanchalsingh1319@gmail.com>
2026-01-29 19:43:41 +0530

    Update 03_fact_sales.sql

--- sql/03_fact_sales.sql
+++ sql/03_fact_sales.sql
@@ -3,3 +3,26 @@
 -- fact_sales table
 -- joins orders, order_items, products, payments
 -- calculates revenue = quantity * price
+
+
+-- FACT table: fact_sales
+-- Grain: one row per product per order (order item)
+
+CREATE OR REPLACE TABLE ANALYTICS.fact_sales AS
+SELECT
+    oi.order_id,
+    o.order_date,
+    o.customer_id,
+    oi.product_id,
+    oi.quantity,
+    p.price,
+    (oi.quantity * p.price) AS revenue,
+    pay.payment_status
+FROM STAGING.stg_order_items oi
+JOIN STAGING.stg_orders o
+    ON oi.order_id = o.order_id
+JOIN STAGING.stg_products p
+    ON oi.product_id = p.product_id
+LEFT JOIN STAGING.stg_payments pay
+    ON oi.order_id = pay.order_id
+WHERE o.order_status = 'DELIVERED';
